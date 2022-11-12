285a5a3c205e847e834dff0bd63ca096b53bc200
cf-stuff <catform99@gmail.com>
2022-11-12 16:05:59 +1100

    preload skill frame

--- js/index.js
+++ js/index.js
@@ -7,13 +7,14 @@ import { getPlayer } from "./templates.js";
 
 render(html`<${App} />`, document.body);
 
-CFDB.getFighters().forEach(fighter => preload(getImagePath(ImageType.fighter, fighter.iconId)));
 CFDB.getSkillTypes().forEach(type => preload(getImagePath(ImageType.skillType, type.iconId)));
 CFDB.getSkills().filter(skill => skill.name !== "Normal Attack")
   .forEach(skill => preload(getImagePath(ImageType.skill, skill.iconId)));
 CFDB.getFighterSkills().forEach(skill => preload(getImagePath(ImageType.fighterSkill, skill.iconIds[2])));
 CFDB.getPetSkills().forEach(skill => preload(getImagePath(ImageType.petSkill, skill.iconId)));
 CFDB.getTotems().forEach(totem => preload(getImagePath(ImageType.totem, totem.iconId)));
+preload("img/display/skill-frame.png");
+preload("img/display/skill-evo-frame.png");
 
 // simulateBattle(
 //   [getPlayer("Brk HH Template"), getPlayer("Tank Template"), getPlayer("Evader Template")],

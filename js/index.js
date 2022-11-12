76a1dcfd72fa1a08b3b7ef2e5a7f924e12fab19f
cf-stuff <catform99@gmail.com>
2022-11-12 15:18:07 +1100

    preload skills

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 import { html, render } from "https://unpkg.com/htm/preact/standalone.module.js";
 import { simulateBattle } from "./battle.js";
 import App from "./components/App.js";
+import CFDB from "./data/CFDB.js";
+import { getImagePath, ImageType, preload } from "./image.js";
 import { getPlayer } from "./templates.js";
 
 render(html`<${App} />`, document.body);
+
+CFDB.getSkills().filter(skill => skill.name !== "Normal Attack")
+  .forEach(skill => preload(getImagePath(ImageType.skill, skill.iconId)));
 
 // simulateBattle(
 //   [getPlayer("Brk HH Template"), getPlayer("Tank Template"), getPlayer("Evader Template")],

98c1539562f28a06604fec26811d06a76d1970ce
cf-stuff <catform99@gmail.com>
2022-11-14 19:45:49 +1100

    buff hit slightly

--- js/config.js
+++ js/config.js
@@ -3,4 +3,4 @@ export const FURY_BURST_THRESHOLD = 300;
 export const PARRY_PERCENT_PER_RES = 0.02;
 export const CRIT_PERCENT_PER_CRT = 0.04;
 export const DODGE_PERCENT_PER_EVA = 0.06;
-export const SKILL_TRIGGER_PERCENT_PER_HIT = 0.01;
+export const SKILL_TRIGGER_PERCENT_PER_HIT = 0.015; // +30% at 2k hit

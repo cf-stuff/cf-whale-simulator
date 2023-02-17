02479a02894c894726ecd4e336a9bdd36d405a86
cf-stuff <catform99@gmail.com>
2023-02-17 20:05:25 +1100

    add step for number inputs

--- js/components/FixedStatInput.js
+++ js/components/FixedStatInput.js
@@ -1,12 +1,15 @@
 import { html } from "https://unpkg.com/htm/preact/standalone.module.js"
+import Stats from "../data/stats.js";
 import NumberInput from "./NumberInput.js";
+
+const decimalStats = [Stats.hpPercent.displayName, Stats.atkPercent.displayName, Stats.spd.displayName];
 
 const FixedStatInput = ({ idPrefix, stat, value, onInput }) => {
   const id = `${idPrefix}-${stat}`;
   return html`
     <div class="input-group">
       <label class="input-group-text" for=${id} style="width:5rem;">${stat}</label>
-      <${NumberInput} id=${id} value=${value} onInput=${onInput} />
+      <${NumberInput} id=${id} value=${value} onInput=${onInput} step=${decimalStats.includes(stat) ? 0.1 : 1} />
     </div>
   `;
 }
--- js/components/NumberInput.js
+++ js/components/NumberInput.js
@@ -1,7 +1,8 @@
 import { html } from "https://unpkg.com/htm/preact/standalone.module.js"
 
-const NumberInput = ({ id, value, onInput, width }) => {
-  return html`<input type="number" id=${id} class="form-control" value=${value} onInput=${onInput} style=${{width}} />`;
+const NumberInput = ({ id, value, onInput, width, step = 1 }) => {
+  return html`<input type="number" id=${id} class="form-control"
+  value=${value} onInput=${onInput} step=${step} style=${{width}} />`;
 }
 
 export default NumberInput;

770f9fd17380436291ece54286f1b71dccf81587
cf-stuff <catform99@gmail.com>
2023-03-25 00:02:16 +1100

    cleanup templates

--- js/templates.js
+++ js/templates.js
@@ -26,6 +26,35 @@ export const getPlayer = name => {
   return Utils.deepClone(player);
 }
 
+const maxAltar = {
+  str: 240,
+  dex: 240,
+  sta: 240,
+  atkPercent: 20,
+  def: 200,
+  eva: 200,
+  hpPercent: 20,
+  hp: 200,
+  crt: 200,
+  spd: 20,
+  brk: 200,
+  hit: 200,
+  res: 200,
+  minAtk: 50,
+  maxAtk: 50
+}
+
+const maxTotemStats = {
+  hit: 120,
+  brk: 120,
+  crt: 120,
+  atkPercent: 12,
+  hpPercent: 12,
+  res: 120,
+  def: 120,
+  eva: 120
+}
+
 Builds.brkTank = {
   name: "Sample BRK Tank",
   level: 100,
@@ -164,36 +193,11 @@ Builds.brkTank = {
       level: 12
     },
   ],
-  altar: {
-    str: 240,
-    dex: 240,
-    sta: 240,
-    atkPercent: 20,
-    def: 200,
-    eva: 200,
-    hpPercent: 20,
-    hp: 200,
-    crt: 200,
-    spd: 20,
-    brk: 200,
-    hit: 200,
-    res: 200,
-    minAtk: 50,
-    maxAtk: 50
-  },
+  altar: maxAltar,
   totem: {
     name: Totems.white.name,
     level: 64,
-    stats: {
-      hit: 120,
-      brk: 120,
-      crt: 120,
-      atkPercent: 12,
-      hpPercent: 12,
-      res: 120,
-      def: 120,
-      eva: 120
-    }
+    stats: maxTotemStats
   },
   skills: [
     Skills.bloodFrenzy.name, Skills.thunderclapQuake.name, Skills.thunderboltBoxing.name,
@@ -345,36 +349,11 @@ Builds.evader = {
       level: 12
     },
   ],
-  altar: {
-    str: 240,
-    dex: 240,
-    sta: 240,
-    atkPercent: 20,
-    def: 200,
-    eva: 200,
-    hpPercent: 20,
-    hp: 200,
-    crt: 200,
-    spd: 20,
-    brk: 200,
-    hit: 200,
-    res: 200,
-    minAtk: 50,
-    maxAtk: 50
-  },
+  altar: maxAltar,
   totem: {
     name: Totems.white.name,
     level: 64,
-    stats: {
-      hit: 120,
-      brk: 120,
-      crt: 120,
-      atkPercent: 12,
-      hpPercent: 12,
-      res: 120,
-      def: 120,
-      eva: 120
-    }
+    stats: maxTotemStats
   },
   skills: [
     Skills.lightningBall.name, Skills.bloodSacrifice.name, Skills.thunderclapQuake.name,
@@ -466,7 +445,7 @@ Builds.tank = {
       name: Gears.tyrantCallousRing.name,
       enhancement: 10,
       stats: [{ res: 140 }, { sta: 110 }, { hp: 194 }, { def: 156 }],
-      gems: [{ name: Gems.topaz.name, plus: 8 }, { name: Gems.emerald.name, plus: 8 }]
+      gems: [{ name: Gems.topaz.name, plus: 9 }, { name: Gems.emerald.name, plus: 9 }]
     }
   ],
   phylactery: {
@@ -526,36 +505,11 @@ Builds.tank = {
       level: 12
     },
   ],
-  altar: {
-    str: 240,
-    dex: 240,
-    sta: 240,
-    atkPercent: 20,
-    def: 200,
-    eva: 200,
-    hpPercent: 20,
-    hp: 200,
-    crt: 200,
-    spd: 20,
-    brk: 200,
-    hit: 200,
-    res: 200,
-    minAtk: 50,
-    maxAtk: 50
-  },
+  altar: maxAltar,
   totem: {
     name: Totems.green.name,
     level: 64,
-    stats: {
-      hit: 120,
-      brk: 120,
-      crt: 120,
-      atkPercent: 12,
-      hpPercent: 12,
-      res: 120,
-      def: 120,
-      eva: 120
-    }
+    stats: maxTotemStats
   },
   skills: [
     Skills.arrivalOfThunderGod.name, Skills.poisonousFog.name, Skills.energyShield.name,
@@ -707,36 +661,11 @@ Builds.hybrid = {
       level: 12
     }
   ],
-  altar: {
-    str: 240,
-    dex: 240,
-    sta: 240,
-    atkPercent: 20,
-    def: 200,
-    eva: 200,
-    hpPercent: 20,
-    hp: 200,
-    crt: 200,
-    spd: 20,
-    brk: 200,
-    hit: 200,
-    res: 200,
-    minAtk: 50,
-    maxAtk: 50
-  },
+  altar: maxAltar,
   totem: {
     name: Totems.white.name,
     level: 64,
-    stats: {
-      hit: 120,
-      brk: 120,
-      crt: 120,
-      atkPercent: 12,
-      hpPercent: 12,
-      res: 120,
-      def: 120,
-      eva: 120
-    }
+    stats: maxTotemStats
   },
   skills: [
     Skills.assassinate.name, Skills.bloodSacrifice.name, Skills.movingIllusion.name,
@@ -750,296 +679,3 @@ Builds.hybrid = {
   arenaTitle: ArenaTitle.grandMaster2.name
 }
 
-Players.trashEvader = {
-  name: "Dingus",
-  level: 100,
-  fighter: {
-    name: Fighters.cc.name,
-    plus: 21,
-    evolved: true,
-    skills: [FighterSkills.thunderclapQuake.name, FighterSkills.pray.name, FighterSkills.bloodFrenzy.name]
-  },
-  pet: {
-    name: Pets.riceball.name,
-    plus: 21,
-    evolved: true,
-    skills: [PetSkills.block.name, PetSkills.riceballSpecialSkill.name, PetSkills.needle.name, PetSkills.energyShield.name]
-  },
-  weapon: Gears.tyrantAgileStaff.name,
-  stats: {
-    hp: 17193,
-    sp: 280,
-    minAtk: 1035,
-    maxAtk: 1195,
-    spd: 735,
-    hit: 1982,
-    eva: 2764,
-    brk: 176,
-    def: 2199,
-    crt: 170,
-    res: 866,
-    furyReversion: 25,
-  },
-  skills: [
-    Skills.bloodFrenzy.name, Skills.woundInfection.name, Skills.thunderclapQuake.name,
-    Skills.magicStealing.name, Skills.shieldWall.name, Skills.cleanse.name
-  ],
-  expertise: [
-    SkillType.water.name, SkillType.fire.name, SkillType.earth.name, SkillType.thunder.name, SkillType.wind.name,
-    SkillType.taijutsu.name, SkillType.ninjutsu.name, SkillType.genjutsu.name, SkillType.healing.name
-  ],
-  resistance: [SkillType.fire.name, SkillType.genjutsu.name],
-  phylactery: {
-    skill: Skills.thunderboltBoxing.name,
-    extraTriggerPercent: 3
-  },
-  totem: {
-    name: Totems.purple.name,
-    level: 64
-  }
-};
-
-Players.tank = {
-  name: "Tank Template",
-  level: 100,
-  fighter: {
-    name: Fighters.madara.name,
-    plus: 21,
-    evolved: true,
-    skills: [FighterSkills.rejuvenation.name, FighterSkills.pray.name, FighterSkills.energyShield.name]
-  },
-  pet: {
-    name: Pets.riceball.name,
-    plus: 21,
-    evolved: true,
-    skills: [PetSkills.block.name, PetSkills.riceballSpecialSkill.name, PetSkills.ignite.name, PetSkills.energyShield.name]
-  },
-  weapon: "Tyrant Callous Sword",
-  stats: {
-    hp: 51116,
-    sp: 756,
-    minAtk: 1377,
-    maxAtk: 1589,
-    spd: 363,
-    hit: 1517,
-    eva: 381,
-    brk: 270,
-    def: 2696,
-    crt: 300,
-    res: 1668,
-    furyReversion: 30
-  },
-  skills: [
-    Skills.energyShield.name, Skills.rejuvenation.name, Skills.earthStyleWall.name,
-    Skills.drawPower.name, Skills.poisonousFog.name, Skills.arrivalOfThunderGod.name
-  ],
-  expertise: [
-    SkillType.water.name, SkillType.fire.name, SkillType.earth.name, SkillType.thunder.name, SkillType.wind.name,
-    SkillType.ninjutsu.name, SkillType.cursedSealJutsu.name, SkillType.genjutsu.name, SkillType.healing.name
-  ],
-  resistance: [SkillType.healing.name, SkillType.genjutsu.name],
-  phylactery: {
-    skill: Skills.movingIllusion.name,
-    extraTriggerPercent: 3
-  },
-  totem: {
-    name: Totems.green.name,
-    level: 64
-  }
-};
-
-Players.critHybrid = {
-  name: "Gumi",
-  level: 100,
-  fighter: {
-    name: Fighters.stryker.name,
-    plus: 21,
-    evolved: true,
-    skills: [FighterSkills.goldenShield.name, FighterSkills.pray.name, FighterSkills.bloodFrenzy.name]
-  },
-  pet: {
-    name: Pets.bat.name,
-    plus: 21,
-    evolved: true,
-    skills: [PetSkills.block.name, PetSkills.batSpecialSkill.name, PetSkills.meditation.name]
-  },
-  weapon: "Tyrant Bloodthirsty Sword",
-  stats: {
-    hp: 16706,
-    sp: 274,
-    minAtk: 2848,
-    maxAtk: 4366,
-    spd: 327,
-    hit: 1943,
-    eva: 351,
-    brk: 1917,
-    def: 2221,
-    crt: 2503,
-    res: 216,
-    furyReversion: 25
-  },
-  skills: [
-    Skills.fireShield.name, Skills.goldenShield.name, Skills.bloodSacrifice.name,
-    Skills.assassinate.name, Skills.movingIllusion.name, Skills.rebirth.name
-  ],
-  expertise: [
-    SkillType.water.name, SkillType.fire.name, SkillType.earth.name, SkillType.thunder.name, SkillType.wind.name,
-    SkillType.ninjutsu.name, SkillType.cursedSealJutsu.name, SkillType.genjutsu.name, SkillType.healing.name
-  ],
-  resistance: [SkillType.thunder.name, SkillType.genjutsu.name],
-  phylactery: {
-    skill: Skills.bloodFrenzy.name,
-    extraTriggerPercent: 3
-  },
-  totem: {
-    name: Totems.white.name,
-    level: 64
-  }
-};
-
-Players.brkTank = {
-  name: "Brk Tank Template",
-  level: 100,
-  fighter: {
-    name: Fighters.madara.name,
-    plus: 21,
-    evolved: true,
-    skills: [FighterSkills.bloodFrenzy.name, FighterSkills.shieldWall.name, FighterSkills.thunderclapQuake.name]
-  },
-  pet: {
-    name: Pets.sleipnir.name,
-    plus: 21,
-    evolved: true,
-    skills: [PetSkills.block.name, PetSkills.sleipnirSpecialSkill.name, PetSkills.energyShield.name, PetSkills.barbarian.name]
-  },
-  weapon: "Tyrant Callous Sword",
-  stats: {
-    hp: 31745,
-    sp: 686,
-    minAtk: 1589,
-    maxAtk: 1819,
-    spd: 401,
-    hit: 2041,
-    eva: 431,
-    brk: 2518,
-    def: 2298,
-    crt: 330,
-    res: 1645,
-    furyReversion: 25
-  },
-  skills: [
-    Skills.bloodFrenzy.name, Skills.thunderclapQuake.name, Skills.thunderboltBoxing.name,
-    Skills.drawPower.name, Skills.goldenShield.name, Skills.bloodSacrifice.name
-  ],
-  expertise: [
-    SkillType.water.name, SkillType.fire.name, SkillType.earth.name, SkillType.thunder.name, SkillType.wind.name,
-    SkillType.ninjutsu.name, SkillType.cursedSealJutsu.name, SkillType.genjutsu.name, SkillType.healing.name
-  ],
-  resistance: [SkillType.healing.name, SkillType.genjutsu.name],
-  phylactery: {
-    skill: Skills.movingIllusion.name,
-    extraTriggerPercent: 3
-  },
-  totem: {
-    name: Totems.white.name,
-    level: 64
-  }
-};
-
-Players.evader = {
-  name: "Evader Template",
-  level: 100,
-  fighter: {
-    name: Fighters.cc.name,
-    plus: 21,
-    evolved: true,
-    skills: [FighterSkills.thunderclapQuake.name, FighterSkills.shieldWall.name, FighterSkills.bloodFrenzy.name]
-  },
-  pet: {
-    name: Pets.rekachu.name,
-    plus: 21,
-    evolved: true,
-    skills: [PetSkills.block.name, PetSkills.rekachuSpecialSkill.name, PetSkills.needle.name, PetSkills.energyShield.name]
-  },
-  weapon: "Tyrant Agile Staff",
-  stats: {
-    hp: 17072,
-    sp: 246,
-    minAtk: 2308,
-    maxAtk: 2554,
-    spd: 725,
-    hit: 2034,
-    eva: 2906,
-    brk: 711,
-    def: 2131,
-    crt: 330,
-    res: 430,
-    furyReversion: 25,
-  },
-  skills: [
-    Skills.bloodFrenzy.name, Skills.assassinate.name, Skills.thunderclapQuake.name,
-    Skills.magicStealing.name, Skills.shieldWall.name, Skills.cleanse.name
-  ],
-  expertise: [
-    SkillType.water.name, SkillType.fire.name, SkillType.earth.name, SkillType.thunder.name, SkillType.wind.name,
-    SkillType.taijutsu.name, SkillType.ninjutsu.name, SkillType.genjutsu.name, SkillType.healing.name
-  ],
-  resistance: [SkillType.thunder.name, SkillType.genjutsu.name],
-  phylactery: {
-    skill: Skills.thunderboltBoxing.name,
-    extraTriggerPercent: 3
-  },
-  totem: {
-    name: Totems.white.name,
-    level: 64
-  }
-};
-
-Players.brkHH = {
-  name: "Brk HH Template",
-  level: 100,
-  fighter: {
-    name: Fighters.stryker.name,
-    plus: 21,
-    evolved: true,
-    skills: [FighterSkills.goldenShield.name, FighterSkills.pray.name, FighterSkills.bloodFrenzy.name]
-  },
-  pet: {
-    name: Pets.sleipnir.name,
-    plus: 21,
-    evolved: true,
-    skills: [PetSkills.block.name, PetSkills.sleipnirSpecialSkill.name, PetSkills.barbarian.name, PetSkills.energyShield.name]
-  },
-  weapon: "Tyrant Bloodthirsty Sword",
-  stats: {
-    hp: 17821,
-    sp: 253,
-    minAtk: 3273,
-    maxAtk: 4917,
-    spd: 401,
-    hit: 2037,
-    eva: 451,
-    brk: 2620,
-    def: 2274,
-    crt: 303,
-    res: 1089,
-    furyReversion: 25
-  },
-  skills: [
-    Skills.fireShield.name, Skills.goldenShield.name, Skills.bloodSacrifice.name,
-    Skills.assassinate.name, Skills.movingIllusion.name, Skills.rebirth.name
-  ],
-  expertise: [
-    SkillType.water.name, SkillType.fire.name, SkillType.earth.name, SkillType.thunder.name, SkillType.wind.name,
-    SkillType.ninjutsu.name, SkillType.cursedSealJutsu.name, SkillType.genjutsu.name, SkillType.healing.name
-  ],
-  resistance: [SkillType.healing.name, SkillType.genjutsu.name],
-  phylactery: {
-    skill: Skills.bloodFrenzy.name,
-    extraTriggerPercent: 3
-  },
-  totem: {
-    name: Totems.white.name,
-    level: 64
-  }
-};

73d34b9519e4d1a60f54afcc08a67fdc48ec42a2
cf-stuff <catform99@gmail.com>
2023-03-30 00:43:54 +1100

    Added changelog

--- js/components/App.js
+++ js/components/App.js
@@ -27,10 +27,26 @@ const App = () => {
     ${activeTab === "Battle" && html`<${Battle} />`}
     ${activeTab === "Grade" && html`<${Grade} />`}
     <hr/>
-    Version 1.5.1<br/>
     Feel free to report any issues<br/>
     LINE ID: derpidgey<br/>
     Discord: CF99#9616
+    <hr/>
+    <h3>Changelog</h3>
+    <p>2023-03-30: 1.5.2 - Fixed esw damage</p>
+    <p>2023-03-28: 1.5.1 - Fixed ice to be more consistent with in game</p>
+    <p>2023-03-27: 1.5.0 - Fixed ordering of secondary skill effects and fury burst/rebirth</p>
+    <p>2023-03-26: 1.4.0 - Added grading; Implemented bomb throw back</p>
+    <p>2023-03-23: 1.3.2 - Display spd tiers and seconds per attack</p>
+    <p>2023-03-10: 1.3.1 - Added international pet icons</p>
+    <p>2023-02-27: 1.3.0 - Import and export builds via code; Sort saved builds in alphabetical order</p>
+    <p>2023-02-18: 1.2.5 - Prevent healing through energy shield</p>
+    <p>2023-02-17: 1.2.4 - Update star altar min and max atk from 40 to 50</p>
+    <p>2023-01-21: 1.2.3 - Added Hel</p>
+    <p>2022-11-16: 1.2.2 - Team battles</p>
+    <p>2022-11-14: 1.2.1 - Added all legendary fighters; Don't allow skills to be used while frozen</p>
+    <p>2022-11-13: 1.2.0 - Added battling</p>
+    <p>2022-11-12: 1.1.0 - Save and load builds locally</p>
+    <p>2022-11-11: 1.0.0 - Created calculator</p>
   </div>
   `;
 }

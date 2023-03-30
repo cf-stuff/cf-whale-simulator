2fa40317a6daa24498011204b22b7ecd2d7624e8
cf-stuff <catform99@gmail.com>
2023-03-30 23:10:08 +1100

    finish planner

--- js/components/SkillPlanner.js
+++ js/components/SkillPlanner.js
@@ -4,20 +4,41 @@ import { getImagePath, ImageType } from "../image.js";
 import SkillSet from "./SkillSet.js";
 
 const SkillPlanner = () => {
-  const [set1, setSet1] = useState([1, 0, 2, 0, 0, 0, 0]);
-  const [set2, setSet2] = useState([0, 0, 0, 0, 0, 0, 0]);
-  const [set3, setSet3] = useState([0, 0, 0, 0, 0, 0, 0]);
+  const [skillsets, setSkillsets] = useState([
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+  ]);
+
+  const setSkill = skillset => {
+    return (position, id) => {
+      const newSkills = skillsets.map(set => [...set]);
+      if (position !== 0) {
+        for (let set of newSkills) {
+          for (let i = 0; i < set.length; ++i) {
+            if (set[i] === Number(id) && i !== 0) {
+              set[i] = 0;
+            }
+          }
+        }
+      }
+      newSkills[skillset][position] = Number(id);
+      setSkillsets(newSkills);
+    }
+  }
+
   return html`
   <div class="row pt-3">
     <div class="skill-container">
     ${CFDB.getSkills().filter(skill => skill.name !== "Normal Attack").map(skill => html`
-    <img class="skill" src=${getImagePath(ImageType.skill, skill.iconId)} />`)}
+    <img class="skill" draggable ondragstart=${e => e.dataTransfer.setData("id", skill.iconId)}
+    src=${getImagePath(ImageType.skill, skill.iconId)} />`)}
     </div>
   </div>
   <div class="row">
-    <div class="col-md pt-3"><${SkillSet} setNumber=1 skills=${set1} /></div>
-    <div class="col-md pt-3"><${SkillSet} setNumber=2 skills=${set2} /></div>
-    <div class="col-md pt-3"><${SkillSet} setNumber=3 skills=${set3} /></div>
+    <div class="col-md pt-3"><${SkillSet} skillsetNumber=1 skills=${skillsets[0]} setSkill=${setSkill(0)} /></div>
+    <div class="col-md pt-3"><${SkillSet} skillsetNumber=2 skills=${skillsets[1]} setSkill=${setSkill(1)} /></div>
+    <div class="col-md pt-3"><${SkillSet} skillsetNumber=3 skills=${skillsets[2]} setSkill=${setSkill(2)} /></div>
   </div>
   `;
 }
--- js/components/SkillSet.js
+++ js/components/SkillSet.js
@@ -1,20 +1,24 @@
-import { html, useState } from "https://unpkg.com/htm/preact/standalone.module.js"
+import { html } from "https://unpkg.com/htm/preact/standalone.module.js"
 import { getImagePath, ImageType } from "../image.js";
 
-const SkillSet = ({setNumber, skills}) => {
+const SkillSet = ({ skillsetNumber, skills, setSkill }) => {
+  const handleDragStart = position => e => {
+    e.dataTransfer.setData("id", skills[position]);
+  }
   return html`
   <div class="skillset">
     <div class="skill-container skills">
-      <div class="skill-frame phy-container text-center">
-        ${skills[0] !== 0 && html`<img class="skill" src=${getImagePath(ImageType.skill, skills[0])} />`}
+      <div class="skill-frame phy-container text-center" ondragover=${e => e.preventDefault()} ondrop=${e => setSkill(0, e.dataTransfer.getData("id"))}>
+        ${skills[0] !== 0 && html`<img class="skill" draggable ondragstart=${handleDragStart(0)} src=${getImagePath(ImageType.skill, skills[0])} />`}
         <img class="phy-indicator" src=${getImagePath(ImageType.skillSlot, "phyIndicator")} />
       </div>
-      <div style="width:75px" class="text-center"><img src=${getImagePath(ImageType.skillSlot, `set${setNumber}`)} /></div>
+      <div style="width:75px" class="text-center"><img class="skillset-number" src=${getImagePath(ImageType.skillSlot, `set${skillsetNumber}`)} /></div>
       <div style="width:75px"></div>
     </div>
     <div class="skill-container skills pt-3">
-      ${skills.slice(1).map(skill => html`<div class="skill-frame text-center">
-      ${skill !== 0 && html`<img class="skill" src=${getImagePath(ImageType.skill, skill)} />`}
+      ${skills.slice(1).map((skill, i) => html`<div class="skill-frame text-center"
+      ondragover=${e => e.preventDefault()} ondrop=${e => setSkill(i + 1, e.dataTransfer.getData("id"))}>
+      ${skill !== 0 && html`<img class="skill" draggable ondragstart=${handleDragStart(i + 1)} src=${getImagePath(ImageType.skill, skill)} />`}
       </div>`)}
     </div>
   </div>

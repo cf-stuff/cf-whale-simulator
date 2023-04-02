73f9fcc2ad6a5af7a40b613d08ed2338739a83cd
cf-stuff <catform99@gmail.com>
2023-04-02 17:40:40 +1000

    gem icons

--- js/components/GearGem.js
+++ js/components/GearGem.js
@@ -1,13 +1,20 @@
 import { html } from "https://unpkg.com/htm/preact/standalone.module.js"
+import CFDB from "../data/CFDB.js";
+import { getImagePath, ImageType } from "../image.js";
 import NumberInput from "./NumberInput.js";
 import SelectInput from "./SelectInput.js";
 
 const GearGem = ({ options, gem, setGem }) => {
+  const id = CFDB.getGem(gem.name)?.iconId;
   return html`
   <div class="input-group">
     <${SelectInput} value=${gem.name} options=${options} onChange=${e => setGem({ ...gem, name: e.target.value })} />
     <span class="input-group-text">+</span>
     <${NumberInput} value=${gem.plus} onInput=${e => setGem({ ...gem, plus: e.target.value })} />
+    <span class="input-group-text">
+      ${id ? html`<img class="icon" src=${getImagePath(ImageType.item, `${id}_${gem.plus + 1}`)} />`
+      : html`<span class="icon" />`}
+    </span>
   </div>
   `;
 }
